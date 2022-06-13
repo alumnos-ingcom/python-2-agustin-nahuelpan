ece9d2513420c4c905f0dfbee1f5014881e9335e
Agustin Nahuelpan <agustin.d.nahuelpan@gmail.com>
2022-06-13 19:38:31 -0300

    La version anterior era horrible. Esta tiene la funcion de identificar caracteres de entrada y salida mejor.

--- src/ejercicio5.py
+++ src/ejercicio5.py
@@ -32,53 +32,29 @@ Hacer que la función verifique el balanceo simultaneo de parentesis,
 llaves y corchetes.
 """
 
-def corchetes_balanceados(cadena_armada, simbolos_regreso):
+def corchetes_balanceados(cadena_armada, segundo, primero):
     """
-    Revisa si los caracteres indicados estan balanceados.
+    La funcion revisa si los corchetes estan desbalanceados.
     """
-    resultado = [] #Una lista para devolver los resultados de la operación.
-    numero_revision = len(cadena_armada) -1 #Un contador del numero de caracteres en la cadena de texto y poder revirla.
-    simbolos_revision = len(simbolos_regreso) -1 #Un contador de la cantidad de simbolos para varios usos.
-    contador_numero_de_signo = 0 #Para ver que numero de signo es el actual.
-    while simbolos_revision >= 0: #Cuando no quedan simbolos para revisar el programa termina.
-        suma=contador_numero_de_signo
-        #suma= suma + 1 #Para identificar al numero siguiente que sería el primer signo (pricipalmente para que quede agradable a la vista en la devolución, es decir, así (),y no así: )()
-        if contador_numero_de_signo == 0: # None: #simbolos_regreso(suma):
-            #simbolos_regreso[suma]
-            agregado = simbolos_regreso[suma]
-            resultado.append(agregado) #Guarda en la variable de salida el primer valor.        
-        contador_simbolo = 1 #Controla el while de abajo.
-        contador_revision = 0 #Ve en que numero de la cadena de texto, de izquierda a derecha, esta revisando.
-        while contador_simbolo > 0:
-            #cadena_armada[contador_revision] #Invica un caracter de la cadena ingresada.
-            
-            if simbolos_regreso[contador_numero_de_signo] == cadena_armada[contador_revision]: #Revisa si el caracter ingresado se corresponde con un caracter de la cadena.
-                contador_simbolo_contrario = contador_revision #Asigna el valor a partir del lugar en donde lo encotntro para empezar a retroceder buscando el caracter que lo cierra.
-                if contador_numero_de_signo ==  0: #simbolos_regreso(contador_numero):
-                    suma = suma + 1
-                    agregado = simbolos_regreso[suma]
-                    resultado.append(agregado) #Guarda en la variable de salida el proximo valor
-                while contador_simbolo_contrario >= 0: #Empieza a revisar los numeros ya registrados buscando el contrario.
-                    comparacion = cadena_armada[contador_simbolo_contrario]
-                    comparación_dos = suma 
-                    if comparacion == comparación_dos:
-                    #if cadena_armada[contador_simbolo_contrario] == simbolos_revision[suma]: #Compara si el numero anterior de la lista es igual al siguiente de los simbollos. Es decir, el que cierra el signo.
-                        cadena_armada[contador_simbolo_contrario] = " " #Elimina el numero ya contado de la cadena
-                        if contador_revision == numero_revision: #Si el numero de caracteres recorridos de la cadena son todos los caracteres.
-                            contador_simbolo = 0
-                            contador_numero_de_signo = contador_numero_de_signo + 1
-                            simbolos_revision = contador_revision - 2 #Termina con el conteo del primer caracter de los que quiere buscar.
-                            contodar_simbolo_contrario = -1
-                            if simbolos_revision == 0: #Si los caracteres que quiere buscar se terminan, determina el resultado como uno verdadero.
-                                resultado.append("true")
-                    contador_simbolo_contrario = contador_simbolo_contrario - 1
-                    if contador_simbolo_contrario == -1: #Si no se encuentra un caracter que cierre para cuando se termina de revisar la cadena, entonces asigna un valor negativo.
-                        resultado.append(False)
-                        contador_simbolo = 0
-                        contador_numero_de_signo = contador_numero_de_signo + 1
-                        simbolos_revision = simbolos_revision - 2
-                    
-            contador_revision = contador_revision + 1
+    numero_caracteres = len(cadena_armada) -1
+    contador = 0
+    primera_suma = 0
+    segunda_suma = 0
+    while contador <= numero_caracteres: #Reviso que tenga la misma cantidad de caracteres.
+        comparacion = cadena_armada[contador]
+        if comparacion == primero:
+            primera_suma = primera_suma + 1
+        if comparacion == segundo:
+            segunda_suma = segunda_suma + 1
+        contador = contador + 1
+    if primera_suma != segunda_suma: #Si no los tiene ya se que es falsa.
+        resultado = False
+    else: #Si los tiene necesito más pruebas.
+        
+        
+        
+        
+        
     return resultado
     
     pass
@@ -86,7 +62,7 @@ def corchetes_balanceados(cadena_armada, simbolos_regreso):
 def armar_cadena(cadena):
     """
     Pide y arma una cadena con los caracteres pedidos.
-#    """
+    """
     #cadena = input("Ingrese una linea o cadena de texto para analizar.")
     cantidad = len(cadena) -1
     numero = 0
@@ -99,43 +75,6 @@ def armar_cadena(cadena):
     
     pass
 
-
-#def simbolos():
-#    """
-#    Pide que ingresen el simbolo o los simbolos que buscar en el programa.
-#    """
-#    simbolos_regreso = []
-#    contador = 1
-#    while contador > 1:
-#        segundo = input("Ingrese el tipo de simbolo que desea buscar para que abra(ejemplo: "<" ): ")
-#        primero = input("Ingrese el tipo de simbolo que desea buscar para que cierre(ejemplo: ">"): :")
-#        simbolos_regreso.append(segundo)
-#        simbolos_regreso.append(primero)
-#        respuesta = int(input("Ingrese 0 si no desea buscar ningun otro simbolo."))
-#        if respuesta == 0:
-#            contador = 0
-#    return simbolos_regreso
-
-    #simbolos = 0
-    #while simbolos < 3: #Convierte lo ingresado en un solo simbolo, que sera lo utilizado por otra función.
-    #    simbolos_regreso[simbolos]
-    #    if simbolos_regreso[simbolos] == "()":
-    #        #")" = simbolos_regreso[simbolos]
-    #        simbolos_regreso[simbolos] = ")"
-    #        simbolos = simbolos + 1
-    #    elif simbolos_regreso[simbolos] == "{}":
-    #        #"}" = simbolos_Rergreso[simbolos]
-    #        simbolos_regreso[simbolos] = "}"
-    #        #simbolos = simbolos + 1
-    #        simbolos = simbolos + 1
-    #    elif simbolos_regreso[simbolos] == "[]":
-    #        #"]" = simbolos_regreso[simbolos]
-    #        simbolos_regreso[simbolos] = "]"
-    #        simbolos = simbolos + 1
-    #return simbolos_regreso
-    
-    pass
-
 def principal():
     """
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
@@ -143,29 +82,16 @@ def principal():
     """
     cadena = input("Ingrese una linea o cadena de texto para analizar: ")
     cadena_armada = armar_cadena(cadena) #Llama a armar_cadena para armar una.
-    
-    simbolos_regreso = []
     contador = 1
     while contador > 0:
         segundo = input("Ingrese el tipo de simbolo que desea buscar para que abra: ")
         primero = input("Ingrese el tipo de simbolo que desea buscar para que cierre: ")
-        simbolos_regreso.append(segundo)
-        simbolos_regreso.append(primero)
+        print("El sigueinte mensaje indicara con un True si estan cerrados o un False si no lo esta.")
+        resultado = corchetes_balanceados(cadena_armada, segundo, primero) #Revisa si el texto enviado tiene los simbolos balanceados.
+        print (f"{resultado}")
         respuesta = int(input("Ingrese 0 si no desea buscar ningun otro simbolo: "))
         if respuesta == 0:
             contador = 0
-    #simbolos_regreso = simbolos() #Llama a simbolos para determinar que buscar.
-    resultado = corchetes_balanceados(cadena_armada, simbolos_regreso) #Revisa si el texto enviado tiene los simbolos balanceados.
-    
-    print(f"El resultado de la operación marca con True cuando los simbolos estan cerrrados, y con False cuando no lo están: {resultado}")
-    
-    #que_devolver = len(resultado) #Para saber cuantos resultados hubieron.
-    #if que_devolver == 3:
-    #    print (f"El simbolo {armar_cadena[0]} es {resultado[0]}. El simbolo{armar_cadena[1]} es {resultado[1]}. Y el simbolo {armar_cadena[2]} es {resultado[2]}.")
-    #if que_devolver == 2:
-    #    print (f"El simbolo {armar_cadena[0]} es {resultado[0]}. Y el simbolo{armar_cadena[1]} es {resultado[1]}.")
-    #if que_devolver == 1:
-    #    print (f"El simbolo {armar_cadena[0]} es {resultado[0]}.")
 
     pass
 
@@ -177,7 +103,4 @@ if __name__ == "__main__":
     armar_cadena(cadena)
     
 if __name__ == "__main__":
-    corchetes_balenceados()
-    
-#if __name__ == "__main__":
-#    simbolos(regreso)
+    corchetes_balanceados(cadena_armada, segundo, primero)
